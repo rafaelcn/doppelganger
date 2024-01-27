d6a4144e71cf5d45f6524205dd45da0b05744383
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-01-27 19:55:55 -0300

    Add concurrency support
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::collections::HashMap;
 use std::{env, fs};
 
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
 mod doppelganger;
 
 use crate::doppelganger::file::File;
 
+#[allow(unused)]
 fn main() -> Result<(), String> {
     let args: Vec<_> = env::args().collect();
 
@@ -16,43 +21,61 @@ fn main() -> Result<(), String> {
 
     match arg {
         Some(directory) => {
-            let mut doppelganger: HashMap<u64, File> = HashMap::new();
+            let doppelganger: HashMap<u64, File> = HashMap::new();
+
+            let mut handles = vec![];
+            let mutex = Arc::new(Mutex::new(doppelganger));
 
             let files = fs::read_dir(directory).expect("failed to read directory");
 
             for entry in files {
-                let dir_entry = entry.expect("failed to capture a directory entry");
+                let resource = mutex.clone();
 
-                let file_name = dir_entry.file_name().to_str().unwrap().to_string();
-                let file_path = dir_entry.path();
-                let file_type = dir_entry.file_type().expect("failed to get the file type");
+                let handle = thread::spawn(move || {
+                    let dir_entry = entry.expect("failed to capture a directory entry");
 
-                if let Some(v) = file_path.to_str() {
-                    if file_type.is_file() {
-                        let mut file = File::new(v.to_string());
+                    let file_name = dir_entry.file_name().to_str().unwrap().to_string();
+                    let file_path = dir_entry.path();
+                    let file_type = dir_entry.file_type().expect("failed to get the file type");
 
-                        file.hash().expect("failed to calculate file hash");
+                    if let Some(v) = file_path.to_str() {
+                        if file_type.is_file() {
+                            let mut file = File::new(v.to_string());
 
-                        if let Some(duplicated) = doppelganger.get(&file.hash) {
-                            file.duplicates.push(file_name);
-                            file.duplicates.append(&mut duplicated.duplicates.clone());
+                            file.hash().expect("failed to calculate file hash");
 
-                            file.duplicates_number += duplicated.duplicates_number + 1;
+                            let mut map = resource
+                                .lock()
+                                .expect("couldn't get a hold in the doppelganger map");
 
-                            doppelganger.insert(file.hash, file);
-                        } else {
-                            file.duplicates.push(file_name);
+                            if map.contains_key(&file.hash) {
+                                let duplicated = map.get(&file.hash).unwrap();
 
-                            doppelganger.insert(file.hash, file);
+                                file.duplicates.push(file_name);
+                                file.duplicates.append(&mut duplicated.duplicates.clone());
+
+                                file.duplicates_number += duplicated.duplicates_number + 1;
+                            } else {
+                                file.duplicates.push(file_name);
+                            }
+
+                            map.insert(file.hash, file);
                         }
                     }
-                }
+                });
+
+                handles.push(handle);
             }
 
-            doppelganger
-                .iter()
-                .filter(|(k, v)| v.duplicates_number >= 1)
-                .for_each(|(k, v)| {
+            for handle in handles {
+                handle.join().unwrap();
+            }
+
+            let map = mutex.lock().unwrap();
+
+            map.iter()
+                .filter(|(_k, v)| v.duplicates_number >= 1)
+                .for_each(|(_k, v)| {
                     println!(
                         "[{}] file is duplicated {} time(s)",
                         v.hash, v.duplicates_number
